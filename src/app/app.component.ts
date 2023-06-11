80f8f1d6de808e61d36c9cb0f61d9f8041dbe676
ajanaantony003 <ajanaantony003@gmail.com>
2023-06-11 15:15:14 +0400

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { formatPercent } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Myproject';
+  httpdata:any
+  slider:any
+  pdt:any
+  value:boolean=false
+  imageObject:any=[]; 
+  pdtarray:any=[]
+  rating:any
+  pdtrating:any
+  pdtcount:any
+  
+  constructor(public http:HttpClient){
+
+  }
+  ngOnInit():void{
+    this.http.get("https://fakestoreapi.com/products")
+    .subscribe(pdts=>{
+        this.httpdata=pdts
+       // console.log(this.httpdata)
+      for(let i=0;i<this.httpdata.length;i++){
+       
+        this.imageObject.push({"image":this.httpdata[i].image,"title":this.httpdata[i].title,"price":this.httpdata[i].price})
+      }
+    })
+
+   // console.log(this.imageObject)
+  
+  }
+  popup(){
+    this.value=true;
+  }
+  popout(){
+    this.value=false;
+   ;
+  }
+  callme(pdts:any){
+    this.value=true;
+    for(let i=0;i<this.httpdata.length;i++){
+      if(pdts.id==this.httpdata[i].id){
+        this.pdt=this.httpdata[i]
+
+        console.log(this.pdt)
+        this.rating=this.httpdata[i].rating
+       this.pdtrating=this.rating.rate
+       this.pdtcount=this.rating.count
+
+    }
+    else if(pdts.id !==this.httpdata[i].id){
+    this.pdtarray.push(this.httpdata[i])
+
+    }
+  }
+ // console.log(this.pdt)
+  }
+  
+    hintclose(){
+      this.value=false;
+     this.pdtarray=[]
+    }
+  
+  
+
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgImageSliderModule } from 'ng-image-slider';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule,NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgImageSliderModule,
+    NgbModule,
+    NgbCarouselModule,
+    MatCardModule,
+    MatListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
